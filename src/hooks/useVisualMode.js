44a181e070150fe977d308353538bf2f9a4803aa
Karl Jensen <jensen@users.noreply.github.com>
2020-10-02 18:35:06 -0700

    Consistency with the use of reducer function
    It is not needed for back since every back call is tied to a click. It is just consistent with the rule that if using dependency on previous state, use the reducer form.

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -2,17 +2,19 @@ import { useState } from "react";
 
 export default function useVisualMode(initial) {
   const [history, setHistory] = useState([initial]);
-
+  
   function transition(mode, replace) {
     setHistory(prev =>
       replace ? [...prev.slice(0, prev.length - 1), mode] : [...prev, mode]
     );
   }
-
+  
   function back() {
-    if (history.length < 2) return;
-    setHistory(prev => [...prev.slice(0, prev.length - 1)]);
+    setHistory(prev => {
+      if (prev.length < 2) return prev;
+      return prev.slice(0, prev.length - 1)
+    });
   }
-
+  
   return { mode: history[history.length - 1], transition, back };
 }
